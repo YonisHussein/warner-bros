828a8817581f1e51def344d539fa4d2f36537b37
YonisHussein <yonismhussein@gmail.com>
2022-06-09 19:01:15 -0400

    setting up questions

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
-const inquirer = require;
+const inquirer = require('inquirer');
 const fs = require('fs');
+const { title } = require('process');
+
+const questions = [
+    {
+        type: input,
+        name: title,
+        message: 'What is the title of your project?',
+    }
+    {
+        type: input,
+        name: description,
+        message: 'What is your description', 
+    }
+    {
+        type: input,
+        name: 
+        message
+    }
+    {
+        type: input
+        name:
+        message
+    }
+    {
+        type: input
+        name:
+        message
+    }
+    {
+        type: input
+        name:
+        message
+    }
+    {
+        type: input
+        name:
+        message
+    }
+    {
+        type: input
+        name:
+        message
+    }
+]
