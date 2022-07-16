177dd9e23e2a84bce15dd358fbd576a0489d7f11
YonisHussein <yonismhussein@gmail.com>
2022-07-16 15:37:40 -0400

    got the questions to prompt

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./generateMarkdown');
+const generateMarkdown = require('./util/generateMarkdown');
 
 
 const questions = [
     {
-        type: input,
-        name: title,
+        type: 'input',
+        name: 'title',
         message: 'What is the title of your project?',
     },
     {
-        type: input,
-        name: description,
+        type: 'input',
+        name: 'description',
         message: 'What is your description', 
     },
     {
-        type: checkbox,
-        name: TableofContents,
+        type: 'checkbox',
+        name: 'TableofContents',
         message: 'Table of Contents',
         choices: ['Usage, License, Contribution, Test, Questions'],
     },
     {
-        type: input,
-        name: Usage,
+        type: 'input',
+        name: 'Usage',
         message: 'What is your Usage?',
     },
     {
-        type: input,
-        name: license,
+        type: 'input',
+        name: 'license',
         message: 'What is your license?',
     },
     {
-        type: input,
-        name: contributing,
+        type: 'input',
+        name: 'contributing',
         message: 'What are you contributing?',
     },
     {
-        type: input,
-        name: Test,
+        type: 'input',
+        name: 'Test',
         message: 'Would you like to run a test?',
     },
     {
-        type: input,
-        name: questions,
+        type: 'input',
+        name: 'questions',
         message: 'Do you have any questions?',
     },
     {
-        type: input,
-        name: github,
+        type: 'input',
+        name: 'github',
         message: 'What is your github username?',
     },
     {
-        type: input,
-        name: email,
+        type: 'input',
+        name: 'email',
         message: 'What is your email?',
     },
 ];
 function writeToFile(fileName, data) {
-fs.writeToFile(`./${fileName}`, data, (err) =>{
-    if(err){
+    fs.writeToFile(`./${fileName}`, data, (err) =>{
+        if(err){
         console.log(err);
         return
     };
@@ -70,4 +70,5 @@ function init() {
 };
 init()
 .then(answers => generateMarkdown(answers))
-.then(generateREADME => writeToFile('README.md', generateREADME));
+.then(generateREADME => writeToFile('README.md', generateREADME))
+;
